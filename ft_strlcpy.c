4f4b0201f832c3b2b14d31f351b3bd9a563bfdea
Err-G <eriklustosa@gmail.com>
2023-10-11 17:59:06 +0100

    review is*, memset, memmove, memcpy, strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,22 +6,22 @@
 /*   By: ecarvalh <ecarvalh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 20:07:41 by ecarvalh          #+#    #+#             */
-/*   Updated: 2023/09/09 20:20:56 by ecarvalh         ###   ########.fr       */
+/*   Updated: 2023/10/11 17:57:51 by ecarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *restrict src, size_t sz)
+size_t	ft_strlcpy(char *dst, const char *restrict src, size_t size)
 {
 	size_t	src_len;
 	size_t	i;
 
 	i = 0;
 	src_len = ft_strlen(src);
-	if (sz <= 0)
+	if (size <= 0)
 		return (src_len);
-	while (src[i] && i < sz - 1)
+	while (src[i] && i < size - 1)
 	{
 		dst[i] = src[i];
 		i++;
