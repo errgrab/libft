661594f617142e7b45ab0e8dab164a7bad0f4344
Err-G <eriklustosa@gmail.com>
2023-10-11 18:37:57 +0100

    review strlcpy and strrchr, make a better libft.h

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,22 +6,22 @@
 /*   By: ecarvalh <ecarvalh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 20:07:41 by ecarvalh          #+#    #+#             */
-/*   Updated: 2023/10/11 17:57:51 by ecarvalh         ###   ########.fr       */
+/*   Updated: 2023/10/11 18:02:04 by ecarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *restrict src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *restrict src, size_t sz)
 {
 	size_t	src_len;
 	size_t	i;
 
 	i = 0;
 	src_len = ft_strlen(src);
-	if (size <= 0)
+	if (sz <= 0)
 		return (src_len);
-	while (src[i] && i < size - 1)
+	while (src[i] && i < sz - 1)
 	{
 		dst[i] = src[i];
 		i++;
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ecarvalh <ecarvalh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 18:36:07 by ecarvalh          #+#    #+#             */
-/*   Updated: 2023/10/09 18:47:22 by ecarvalh         ###   ########.fr       */
+/*   Updated: 2023/10/11 18:09:26 by ecarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-/* Bonus */
+/* Bonus typedef */
 
 typedef struct s_list
 {
@@ -31,46 +31,63 @@ void	ft_bzero(void *s, size_t n);
 
 void	*ft_calloc(size_t nmemb, size_t size);
 
+/* mem_ */
+
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
-size_t	ft_strlen(const char *s);
+/* str_ */
 
 char	*ft_strdup(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *s1, const char *s2, size_t n);
+size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t sz);
 size_t	ft_strlcat(char *dst, const char *src, size_t sz);
-char	*ft_strnstr(const char *s1, const char *s2, size_t n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+
+/* is_ */
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+
+/* to_ */
+
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 
 /* Part 2 */
 
+char	**ft_split(char const *s, char c);
+
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+char	*ft_itoa(int n);
+
+/* str_ */
+
 char	*ft_strjoin(char const *s1, char const*s2);
 char	*ft_strtrim(char const *s1, char const *set);
-char	**ft_split(char const *s, char c);
-char	*ft_itoa(int n);
 char	*ft_strmapi(char const*s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+
+/* put_ */
+
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char*s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
 /* Bonus */
+
+/* lst_ */
 
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
