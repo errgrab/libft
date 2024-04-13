b361d40c52ca7c7ccd5c566326ae424f27afc71b
ErikG <err-g@users.noreply.github.com>
2024-04-13 20:03:18 +0100

    add ternary function

--- src/stdlib/ft_tern.c
+++ src/stdlib/ft_tern.c
@@ -1,21 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   stdlib.h                                           :+:      :+:    :+:   */
+/*   ft_tern.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ecarvalh <ecarvalh@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ecarvalh <ecarvalh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/23 00:31:59 by ecarvalh          #+#    #+#             */
-/*   Updated: 2024/04/13 20:02:26 by ecarvalh         ###   ########.fr       */
+/*   Created: 2024/04/13 20:01:56 by ecarvalh          #+#    #+#             */
+/*   Updated: 2024/04/13 20:01:57 by ecarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef STDLIB_H
-# define STDLIB_H
+#include "libft.h"
 
-int		ft_atoi(const char *str);
-char	*ft_itoa(int n);
-void	*ft_calloc(size_t nmemb, size_t size);
-size_t	ft_tern(size_t cond, size_t a, size_t b);
-
-#endif
+size_t	ft_tern(size_t cond, size_t a, size_t b)
+{
+	if (cond)
+		return (a);
+	return (b);
+}
